97210532096bccccf964a11c1646ba1e05d66dbf
stoyanov <stoyanov@itwm.fraunhofer.de>
2019-03-06 15:35:24 +0100

    linking a static libstdc++

--- examples/example0.cpp
+++ examples/example0.cpp
@@ -9,10 +9,25 @@
 main(int argc, char* argv[])
 {
 
-  std::string myMessage("Hi");
-  comprEx::MyClass myClass(myMessage);
+  try {
 
-  myClass.printMessage();
+    gaspi::Runtime runtime;
+
+    gaspi::Context context;
+
+    gaspi::segment::Segment segment(1024*1024);
+
+
+    std::string myMessage("Hi");
+    comprEx::MyClass myClass(myMessage);
+    myClass.printMessage();
+
+
+    return EXIT_SUCCESS;
+  } // try
+  catch(...) {
+     return EXIT_FAILURE;
+  }
 
   return 0;
 }
--- include/comprexchange.hxx
+++ include/comprexchange.hxx
@@ -7,6 +7,14 @@
 #include <iostream>
 #include <string>
 
+#include <GaspiCxx/Runtime.hpp>
+#include <GaspiCxx/Context.hpp>
+#include <GaspiCxx/group/Group.hpp>
+#include <GaspiCxx/segment/Segment.hpp>
+#include <GaspiCxx/singlesided/write/SourceBuffer.hpp>
+#include <GaspiCxx/singlesided/write/TargetBuffer.hpp>
+#include <GaspiCxx/utility/ScopedAllocation.hpp>
+
 #ifndef COMPREX_H
 #define COMPREX_H
 
